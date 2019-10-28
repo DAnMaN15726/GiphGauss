a939ab393be477844423c08a8f0a4c5656102449
danman15726 <danman15726@gmail.com>
2019-10-27 23:09:09 -0500

    Added a function that displays the images of every topic selected. IT DOES NOT WORK BECAUSE OF A CONTENT BLOCK SYSTEM IN THE CONSOLE LOG. WILL FIX LATER.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,6 @@
 const topics = ["gaming", "music", "horror", "funny"];
 
 
-
 let system = 
 {
     buttonPrint: function(inputA){
@@ -10,6 +9,7 @@ let system =
 
             let button = $("<button>" + inputA[i] + "</button>");
             button.attr("id", i);
+            button.attr("value", inputA[i]);
             $(".buttonContainer").append(button);
 
         }
@@ -20,6 +20,38 @@ let system =
 
 
     displayImages: function(topicInput){
+        let search = topicInput;
+        const queryURL = `https://api.giphy.com/v1/gifs/search?api_key=OVaM1ofUqUE26I2l1H5ec6voBazampFz&q=${search}&limit=10&offset=0&rating=G&lang=en`
+
+
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+            for(let i = 0; i < 10; i++){
+
+                const imgURL = response.data[i].embed_url;
+                const image = $("<img>");
+                image.attr("src", imgURL);
+                image.attr("alt", i);
+                $(".results").append(image);
+
+
+            }
+
+
+
+        });
+
+
+
+
+
+
+
+
 
         
 
@@ -42,5 +74,22 @@ let system =
 
 
 system.buttonPrint(topics);
+system.displayImages("Terry Crews");
 
-    
+
+
+
+
+$(".buttonContainer").on("click", function() {
+    console.log("Registered Click");
+    $(".results").empty();
+    // system.displayImages(this.val());
+    let array = [$(this).attr('id')];
+    system.buttonPrint(array);
+    console.log($(this).attr('id'));
+
+
+
+
+
+});
